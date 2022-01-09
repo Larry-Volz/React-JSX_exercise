f0a8934ad51ef0130e0dc7d0cf1c96d54069e3bf
Larry Volz <imaginologist@gmail.com>
2022-01-09 05:40:27 -0500

    part 2 working - double check

---
+++ part-2/App.js
@@ -0,0 +1,9 @@
+const App = () => {
+    return(
+        <dir>
+            <Tweet username="user1" name="Larry" date="today" message="tweet message 1" />
+            <Tweet username="user2" name="Zach" date="tomorrow1" message="tweet message 2" />
+            <Tweet username="user3" name="Faniel" date="tomorrow2" message="tweet message 3" />
+        </dir>
+    )
+}
